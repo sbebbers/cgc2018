c738dfc39ab1f6702a7325e910ec26dd7beeafac
Shaun Bebbington <shaun@square-circle.co.uk>
2018-03-22 15:15:07 +0000

    Latest version with some refactoring.

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,15 +1,1 @@
-/**
- * This script handles page load events on all pages
- */
-$(function(){
-	$(".flash").fadeOut(2e3);
-	$("ul#nav-items > li").on("click touch",function(){
-		let link = $(this).html().split("\"");
-		let i = 0;
-		for(i; i < link.length; i++){
-			if(link[i].substr(0,4) == 'http'){
-				window.location.href = link[i];
-			}
-		}
-	});
-});
+$(function(){$(".flash").fadeOut(2e3);$("ul#nav-items > li").on("click touch",function(){let link = $(this).html().split("\"");let i = 0;for(i; i < link.length; i++){if(link[i].substr(0,4) == 'http'){window.location.href = link[i];}}});});
