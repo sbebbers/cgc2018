3067834fd2eefa22b923271ca8fc3ca4e215324f
Shaun Bebbington <shaun@square-circle.co.uk>
2018-03-30 16:00:56 +0100

    Added in a CGC Adventures sub-section

--- application/controller/AdventuresController.php
+++ application/controller/AdventuresController.php
@@ -1,30 +1,23 @@
 <?php
-use Application\Controller\ControllerCore;
-use Application\Model\Read\ReviewsModel;
 
-require_once(serverPath("/model/read/ReviewsModel.php"));
+require_once(serverPath("/controller/ReviewsController.php"));
 
-class ReviewsController extends ControllerCore
+class AdventuresController extends ReviewsController
 {
     /**
      * @var string $header
      */
-    protected $header = 'Crap Games Competition 2018 Reviews';
+    protected $header = 'CGC Adventures';
     
     /**
      * @var string $subHeader
      */
-    protected $subHeader = 'Here you will find the latest reviews of the very best entries';
+    protected $subHeader = 'You are looking at a website. The website seems rather crap. Below this text you will find reviews for your perusal. What now?';
     
-    public function __construct(bool $reviewsPage = true){
-        ControllerCore::__construct();
-        $this->sql = new ReviewsModel();
+    public function __construct(){
+        ReviewsController::__construct(false);
         $this->view = new stdClass();
-        
-        if($reviewsPage === true){
-            $this->setView();
-        }
-        
+        $this->setView();
         if(empty($this->view->hrClasses)){
             $this->view->hrClasses = $this->getHRClasses();
         }
